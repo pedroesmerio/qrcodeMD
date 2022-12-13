c357a8073243d949f1ce3a0a73db2f04818b5ecb
Pedro Esmerio <p.h.esmerio@gmail.com>
2022-12-13 14:42:13 -0300

    Qr GEnerated

--- generateQRcode.py
+++ generateQRcode.py
@@ -4,24 +4,23 @@ from PIL import Image
 
 # taking image which user wants
 # in the QR code center
-Logo_link = 'Logo01.jpg'
+# Logo_link = 'Logo01.jpg'
 
-logo = Image.open(Logo_link)
+# logo = Image.open(Logo_link)
 
 # taking base width
 basewidth = 100
 
 # adjust image size
-wpercent = (basewidth/float(logo.size[0]))
-hsize = int((float(logo.size[1])*float(wpercent)))
-logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
-
+# wpercent = (basewidth/float(logo.size[0]))
+# hsize = int((float(logo.size[1])*float(wpercent)))
+# logo = logo.resize((basewidth, hsize), Image.ANTIALIAS)
 QRcode = qrcode.QRCode(
 	error_correction=qrcode.constants.ERROR_CORRECT_H
 )
 
 # taking url or text
-url = 'https://merecidodocinho.com'
+url = 'https://pedroesmerio.vercel.app/'
 
 # adding URL or text to QRcode
 QRcode.add_data(url)
@@ -37,11 +36,11 @@ QRimg = QRcode.make_image(
 	fill_color=QRcolor, back_color="white").convert('RGB')
 
 # set size of QR code
-pos = ((QRimg.size[0] - logo.size[0]) // 2,
-	(QRimg.size[1] - logo.size[1]) // 2)
-QRimg.paste(logo, pos)
+# pos = ((QRimg.size[0] - logo.size[0]) // 2,
+# 	(QRimg.size[1] - logo.size[1]) // 2)
+# QRimg.paste(logo, pos)
 
 # save the QR code generated
-QRimg.save('MDocinhoQR.png')
+QRimg.save('portfolio.png')
 
 print('QR code generated!')
